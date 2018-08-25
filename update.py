df1d5c61c89077a1a7549d9eff6010bc79fd0051
Jake <jzilch280@gmail.com>
2018-08-25 13:18:45 -0500

    Added printouts to update script

--- update.py
+++ update.py
@@ -2,8 +2,12 @@
 
 import os
 
+print("Fetching upstream")
 os.system("git fetch upstream")
+print("Updating master with upstream changes")
 os.system("git checkout master")
 os.system("git merge upstream/master")
+print("Updating develop with upstream changing")
 os.system("git checkout develop")
 os.system("git merge upstream/develop")
+print("Updated local repository.\nNow on develop.")
