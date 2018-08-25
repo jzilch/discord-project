c49f9d37b9a6deb620682914270410b1b7cef306
David Maness <davidmaness@Davids-MacBook-Pro.local>
2018-08-25 14:18:50 -0500

    routed root url to index.html; can now render homepage

--- discordapp/views.py
+++ discordapp/views.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.template import RequestContext, Template
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.template.context_processors import csrf
+from django.db import connection, transaction
+from discordapp.models import *
+
+import json
 
 # Create your views here.
+
+
+@csrf_protect
+def render_homepage(request):
+
+    context = {
+        "csrf": csrf,
+        "text": "butts",
+    }
+
+    return render(request, 'index.html', context)
