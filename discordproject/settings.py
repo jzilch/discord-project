ded158214c17ad5611db0e6951761a4b0f09dde0
David Maness <davidmaness@Davids-MacBook-Pro.local>
2018-08-25 19:55:14 -0500

    create base.html; refactor index.html to extend the base; add bootstrap to app

--- discordproject/settings.py
+++ discordproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap_ui',
+    'bootstrap3',
     'discordapp',
 ]
 
