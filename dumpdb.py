2e9e230086230c8a605a78f75a5f8a8961c7e47c
David Maness <davidmaness@Davids-MacBook-Pro.local>
2018-09-04 22:07:13 -0500

    rename refreshdb.py to builddb.py; add dumpdb.py

--- dumpdb.py
+++ dumpdb.py
@@ -3,4 +3,4 @@
 from os import system
 
 system("python manage.py dumpdata discordapp > discordapp/fixtures/initial_data_fixture.json")
-print("Dumped \"superfam\" database to discordapp/fixtures/initial_data_fixture.json.")
+print("\nDONE: Dumped \"superfam\" database!\nFind it in discordapp/fixtures/initial_data_fixture.json.\n")
